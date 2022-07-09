d3c28ba1a06e170d7cb116dd488589efc9eb730d
Oscar <arieloscar1218@gmail.com>
2022-07-09 12:02:02 -0300

    Clase 03 completada

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const btn = document.querySelector('[data-formulario-boton]');
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-formulario-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = "";
+    const content= `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+    console.log(content);
+    task.innerHTML = content;
+    list.appendChild(task);
+    
 }
 
 btn.addEventListener('click', createTask);
