f4844106693bc0b92c1249813e83ed8baa16a432
Oscar <arieloscar1218@gmail.com>
2022-07-13 12:45:33 -0300

    clase04 completada

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const btn = document.querySelector('[data-formulario-boton]');
-///Arrow functions o funciones flechas o funciones anonimas
+(() => { const btn = document.querySelector('[data-formulario-boton]');
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-formulario-input]');
@@ -8,16 +7,38 @@ const createTask = (evento) => {
     const task = document.createElement('li');
     task.classList.add('card');
     input.value = "";
-    const content= `<div>
-    <i class="far fa-check-square icon"></i>
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const taskTitule = document.createElement('span');
+    taskTitule.classList.add('task');
+    taskTitule.innerHTML = value;
+    taskContent.appendChild(taskTitule)
+    const content= `
     <span class="task">${value}</span>
   </div>
   <i class="fas fa-trash-alt trashIcon icon"></i>`
     console.log(content);
-    task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
     
 }
-
+///Arrow functions o funciones flechas o funciones anonimas
 btn.addEventListener('click', createTask);
+
+const checkComplete = () => {
+  const i = document.createElement('i');
+  i.classList.add('far', 'fa-check-square', 'icon'); //Es el equivalente a: ''far fa-check-square icon''
+  i.addEventListener('click',completeTask);                                           
+  return i;  
+}
+
+const completeTask = (event) => {
+  //console.log(event.target);
+  const element = event.target;
+  element.classList.toggle('fas'); // toggle: si existe la clase la 'remove' pero si no está 'add' .
+  element.classList.toggle('completeIcon'); // completeIcon es una clase definida por mi en style.css
+  element.classList.toggle('far');
+}
+// iife (expresiones de función ejecutadas inmediatamente)
+})();
 
